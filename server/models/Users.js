fe1d1bf94bd8ae90574d1fba192ae80d9c3af021
Sarthak Sachdev <saarsaach30@gmail.com>
2025-03-18 14:49:44 +0530

    Update Users.js

--- server/models/Users.js
+++ server/models/Users.js
@@ -1,9 +1,23 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    age: Number
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+    username: {
+        type: String,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    list: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'List'
+    }]
 })
 
 const UserModel = mongoose.model("users", UserSchema);
